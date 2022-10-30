2b3a1555e71b95dd83947e7d5f279bfd265f3cbe
baruch assif <baruchao@amazon.com>
2022-10-30 15:07:04 +0200

    add LICENSE file add more tools to mysql

---
+++ mysql/query_timing_profile.sql
@@ -0,0 +1,19 @@
+-- file: query_timing_profile.sql
+-- Running query profiling on MySQL
+-- Tested on Aurora MySQL,MySQL,RDS MySQL 
+
+
+-- start profiling
+set profiling = 1; 
+-- run query 
+-- 
+
+show profiles; 
+-- will show all profiles with queries 
+-- show all profile for selected query 
+show profile for query 1;
+-- full info 
+show profile ALL for query 1\G
+
+-- stop profiling
+set profiling = 0; 
